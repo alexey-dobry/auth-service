587aff10ba705dcd18ac356cb6a785e804c33c15
Alexey Nehaenko <Nehaenkog23@gmail.com>
2025-07-26 11:54:19 +0300

    rewrite logger

--- auth-service/internal/logger/logger.go
+++ auth-service/internal/logger/logger.go
@@ -1,54 +1,15 @@
 package logger
 
-import (
-	"log"
-	"os"
-	"path/filepath"
-
-	"go.uber.org/zap"
-	"go.uber.org/zap/zapcore"
-)
-
-const (
-	EnvLocal = "local"
-	EnvDev   = "dev"
-	EnvProd  = "prod"
-)
-
-const logDirPath = "./logs"
-
-func NewLogger(env string) *zap.SugaredLogger {
-	os.MkdirAll(logDirPath, os.ModePerm)
-
-	logFile, err := os.OpenFile(filepath.Join(logDirPath, "auth.log"), os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0600)
-	if err != nil {
-		log.Fatalf("Failed to create log file: %s", err.Error())
-	}
-
-	var logCfg zapcore.EncoderConfig
-	switch env {
-	case EnvLocal:
-		logCfg = zap.NewDevelopmentEncoderConfig()
-	case EnvDev:
-		logCfg = zap.NewDevelopmentEncoderConfig()
-	case EnvProd:
-		logCfg = zap.NewProductionEncoderConfig()
-	}
-
-	logCfg.EncodeTime = zapcore.ISO8601TimeEncoder
-
-	fileEncoder := zapcore.NewJSONEncoder(logCfg)
-
-	logCfg.EncodeLevel = zapcore.CapitalColorLevelEncoder
-
-	consoleEncoder := zapcore.NewConsoleEncoder(logCfg)
-
-	cores := []zapcore.Core{
-		zapcore.NewCore(fileEncoder, zapcore.AddSync(logFile), zapcore.DebugLevel),
-		zapcore.NewCore(consoleEncoder, zapcore.AddSync(os.Stdout), zapcore.DebugLevel),
-	}
-
-	core := zapcore.NewTee(cores...)
-
-	return zap.New(core).Sugar()
+type Logger interface {
+	Info(args ...interface{})
+	Infof(template string, args ...interface{})
+	Warn(args ...interface{})
+	Warnf(template string, args ...interface{})
+	Error(args ...interface{})
+	Errorf(template string, args ...interface{})
+	Debug(args ...interface{})
+	Debugf(template string, args ...interface{})
+	Fatal(args ...interface{})
+	Fatalf(template string, args ...interface{})
+	WithFields(args ...interface{}) Logger
 }
