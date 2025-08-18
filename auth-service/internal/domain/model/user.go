3ac5d0ccc3c6ed6e6ef29ee8d5a42ae310d45719
Alexey Nehaenko <Nehaenkog23@gmail.com>
2025-08-18 10:28:21 +0300

    Fix: change user struct

---
+++ auth-service/internal/domain/model/user.go
@@ -0,0 +1,20 @@
+package model
+
+import (
+	"github.com/alexey-dobry/goauth/internal/domain/validator"
+	"gorm.io/gorm"
+)
+
+type User struct {
+	gorm.Model
+	Username     string `gorm:"not null" validate:"required,min=1,max=200"`
+	Email        string `gorm:"not null" validate:"required,email"`
+	HashPassword string `gorm:"not null" validate:"required,sha512"`
+	FirstName    string `gorm:"not null" validate:"required,min=1,max=200"`
+	LastName     string `gorm:"not null" validate:"required,min=1,max=200"`
+	IsAdmin      bool   `gorm:"not null" validate:"required"`
+}
+
+func (u *User) Validate() error {
+	return validator.V.Struct(u)
+}
