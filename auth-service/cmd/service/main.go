d56874a99bc7f1ab78f919799bbef8e43733f0fa
Alexey Nehaenko <Nehaenkog23@gmail.com>
2025-08-20 19:02:33 +0300

    Feat: add service start

--- auth-service/cmd/service/main.go
+++ auth-service/cmd/service/main.go
@@ -1,20 +1,41 @@
 package main
 
 import (
+	"context"
+	"flag"
+
 	"github.com/alexey-dobry/auth-service/internal/app"
 	"github.com/alexey-dobry/auth-service/internal/config"
-	"github.com/alexey-dobry/auth-service/internal/logger"
+	"github.com/alexey-dobry/auth-service/internal/domain/jwt"
+	pg "github.com/alexey-dobry/auth-service/internal/repository/postgresql"
+	"github.com/alexey-dobry/auth-service/internal/server/grpc"
+	"github.com/alexey-dobry/auth-service/pkg/logger/zap"
 )
 
 func main() {
+	flag.Parse()
+
 	cfg := config.MustLoad()
 
-	logger := logger.NewLogger(cfg.Env)
+	logger := zap.NewLogger(cfg.Logger)
 
 	logger.Info("Successfully initialized logger")
 
-	application := app.New(logger, cfg.GRPC.Port, cfg.TockenTTL)
+	repository, err := pg.New(cfg.Repository)
+	if err != nil {
+		logger.Error("Failed to create user repository: %s", err)
+		panic("Failed to create user repository")
+	}
 
-	application.GRPCSrv.Run()
+	jwtHandler, err := jwt.NewHandler(cfg.JWT)
+	if err != nil {
+		logger.Error("Failed to create jwt handler: %s", err)
+		panic("Failed to create jwt handler")
+	}
 
+	authServer := grpc.NewServer(logger, repository, jwtHandler)
+
+	application := app.New(cfg, logger, repository, authServer)
+
+	application.Run(context.Background())
 }
